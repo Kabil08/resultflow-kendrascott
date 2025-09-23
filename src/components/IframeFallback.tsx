076a925978828ed6267ea9f5b2fcdd51cc04a39e
kabil-viome <c_kabil.nagarajan@viome.com>
2025-09-23 17:38:31 +0530

    Image change

--- src/components/IframeFallback.tsx
+++ src/components/IframeFallback.tsx
@@ -4,7 +4,7 @@ export function IframeFallback() {
       className="relative flex flex-col items-center justify-center min-h-screen bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage:
-          'url("https://res.cloudinary.com/dbtapyfau/image/upload/v1758609924/Screenshot_2025-09-23_at_12.14.55_PM_pilhrp.png")',
+          'url("https://res.cloudinary.com/dbtapyfau/image/upload/v1758629285/Screenshot_2025-09-23_at_5.37.29_PM_ipuupg.png")',
       }}
     ></div>
   );
